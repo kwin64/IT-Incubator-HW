ce9ecd290c942912c463b173c096f6f13a66035f
kwin11bvb@yandex.by <kwin11bvb@yandex.by>
2021-03-25 16:22:53 +0300

    added router

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import s from './App.module.css'
 import HW5 from "../../../p2-homeworks/h5/HW5";
-import HW4 from "../../../p2-homeworks/h4/HW4";
 
 function App() {
     return (
@@ -10,7 +9,7 @@ function App() {
             {/*<HW1/>*/}
             {/*<HW2/>*/}
             {/*<HW3/>*/}
-            <HW4/>
+            {/*<HW4/>*/}
             <HW5/>
         </div>
     )
--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Routes";
+import s from './Header.module.css'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.header}>
+            <NavLink to={PATH.PRE_JUNIOR} className={s.path}>Pre Junior</NavLink>
+            <NavLink to={PATH.JUNIOR} className={s.path}>Junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS} className={s.path}>Junior +</NavLink>
         </div>
     );
 }
